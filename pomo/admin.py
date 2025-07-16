c12e93f657ec6268aa7eabfc1a71160095b19c70
YOO GIJUN <yoogijun@YOOs-MacBook-Air.local>
2025-07-17 00:20:34 +0900

    pomo 타이머 동작(no css)

--- pomo/admin.py
+++ pomo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from pomo.models import Pomo
 
 # Register your models here.
+@admin.register(Pomo)
+class PomoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'time', 'created')
--- pomo/models.py
+++ pomo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pomo(models.Model):
+    time = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pomo #{self.id} {self.created.strftime('%Y/%m/%d %H:%M')}"
