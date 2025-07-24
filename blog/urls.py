b3ea8d629d19533129d7850db04610cedce9d736
YOO GIJUN <yoogijun@YOOs-MacBook-Air.local>
2025-07-24 15:05:33 +0900

    app_name 지정, {% url %} 적용

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from blog.views import blog_list, view_blog, blog_add
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'blog'
 urlpatterns = [
     path('', blog_list),
-    path('<int:post_id>/', view_blog),
+    path('<int:post_id>/', view_blog, name='view-blog'),
     path('add/', blog_add),
 ]
